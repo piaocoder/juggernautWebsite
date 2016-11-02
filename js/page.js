71a72c0fbcbb72ff7e6700a4714a0d9536d6e400
xiaoqiang001 <kang991005214@163.com>
2016-11-02 18:17:49 +0800

    完善右侧固定定位逻辑和整体js

--- js/page.js
+++ js/page.js
@@ -6,13 +6,26 @@
 	$(window).on('resize',function(){
 		h=$(window).height();
 	})
-	// 
+
+	// 执行主函数页面切换效果；
 	function changePage(num){
-		if(num <= 0){
+		// 判断执行值是否超过范围
+		if(num < 1){
+			$('#sidebar p.arrow-up').addClass('disabled');
 			num = 0.5;
-		}else if(num > $(".mainbox>div").size()){
-			num = Math.floor($(".mainbox>div").size() - 1)+0.5
+		}else {
+			$('#sidebar p.arrow-up').removeClass('disabled');
 		}
+		// console.log($(".mainbox>div").size())
+		if(num >= $(".mainbox>div").size() - 1){
+			$('#sidebar p.arrow-down').addClass('disabled');
+			num = Math.floor($(".mainbox>div").size() - 1)+0.5
+		}else{
+			$('#sidebar p.arrow-down').removeClass('disabled');
+		}
+		num = Math.floor(num)+0.5;
+		// console.log(num)
+
 		$('.header .nav ul li').eq(Math.floor(num)).addClass("checked").siblings().removeClass()
 		// console.log(num)
 		// console.log(- h * num);
@@ -22,14 +35,23 @@
 		$(".positionFixed").stop().animate({
 			top: h * Math.floor(num)
 		}, 200);
-		return Math.floor(num)+0.5;
+		return num;
 	}
+	changePage(pageNum)
 	$('.header .nav ul li').click(function(){
 		var idx = $('.header .nav ul li').index(this);
-		// console.log(idx);
+		console.log(idx);
 		pageNum = changePage(idx);
 		// console.log(pageNum)
 		// pageNum = idx;
+	})
+	$('#sidebar p.arrow-up').click(function(){
+		pageNum--;
+		pageNum = changePage(pageNum)
+	})
+	$('#sidebar p.arrow-down').click(function(){
+		pageNum++;
+		pageNum = changePage(pageNum)
 	})
 	$(document).on("mousewheel DOMMouseScroll", function(e) {
 		var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || // chrome & ie

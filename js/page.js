93a9c0628c378cef03101fc50d0b47e58e28a16e
unknown <326560318@qq.com>
2016-11-03 17:56:46 +0800

    jibenwancheng

--- js/page.js
+++ js/page.js
@@ -2,51 +2,13 @@
 (function(){
 	var pageNum = 0.5;
 	var flg = true;
-	
-
-	changePage(pageNum);
-	$('.header .nav ul li').click(function(){
-		var idx = $('.header .nav ul li').index(this);
-		// console.log(idx);
-		pageNum = changePage(idx);
-		// console.log(pageNum)
-		// pageNum = idx;
-	});
-	$('.header .logo').click(function(){
-		pageNum = changePage(0);
+	var h = $(window).height();
+	$(window).on('resize',function(){
+		h=$(window).height();
 	})
-	$('#sidebar p.arrow-up').click(function(){
-		pageNum--;
-		pageNum = changePage(pageNum)
-	})
-	$('#sidebar p.arrow-down').click(function(){
-		pageNum++;
-		pageNum = changePage(pageNum)
-	})
-	$(document).on("mousewheel DOMMouseScroll", function(e) {
-		var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || // chrome & ie
-			(e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1)); // firefox
-		// console.log(delta);
-		if(flg){
-			pageNum -= delta;
-			pageNum = changePage(pageNum);
-			// console.log(pageNum);
-			flg = false;
-
-			setTimeout(function() {
-				flg = true;
-			}, 750);
-		}
-	})
-})()
-
 
 	// 执行主函数页面切换效果；
 	function changePage(num){
-		var h = $(window).height();
-		$(window).on('resize',function(){
-			h=$(window).height();
-		})
 		// 判断执行值是否超过范围
 		if(num < 1){
 			$('#sidebar p.arrow-up').addClass('disabled');
@@ -75,3 +37,35 @@
 		}, 200);
 		return num;
 	}
+	changePage(pageNum)
+	$('.header .nav ul li').click(function(){
+		var idx = $('.header .nav ul li').index(this);
+		// console.log(idx);
+		pageNum = changePage(idx);
+		// console.log(pageNum)
+		// pageNum = idx;
+	})
+	$('#sidebar p.arrow-up').click(function(){
+		pageNum--;
+		pageNum = changePage(pageNum)
+	})
+	$('#sidebar p.arrow-down').click(function(){
+		pageNum++;
+		pageNum = changePage(pageNum)
+	})
+	$(document).on("mousewheel DOMMouseScroll", function(e) {
+		var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || // chrome & ie
+			(e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1)); // firefox
+		// console.log(delta);
+		if(flg){
+			pageNum -= delta;
+			pageNum = changePage(pageNum);
+			// console.log(pageNum);
+			flg = false;
+
+			setTimeout(function() {
+				flg = true;
+			}, 750);
+		}
+	})
+})()
---
+++ js/product.js
@@ -0,0 +1,28 @@
+!function(){
+	var  i=0;
+	var timer;
+	var slideSize=$(".product ul>li").size();
+	function autoplay(){
+			timer=setInterval(function(){
+			$(".next").click()
+		},5000);
+	}
+autoplay()
+	function clear(){
+		clearInterval(timer)
+	}
+	$(".product ul").hover(function(){clear()},function(){autoplay()})
+	$(".next").click(function(){
+		i++;
+		if(i>=slideSize)i=0;
+		$(".product ul>li").eq(i).css("opacity",1).siblings().css("opacity",0)
+		$(".product ul>li").eq(i).find("h3,.product-show img").addClass("slide-active").end().siblings().find("h3,.product-show img").removeClass("slide-active")
+	})
+		$(".prev").click(function(){
+		i--;
+		if(i<=0)i=slideSize;
+		$(".product ul>li").eq(i).css("opacity",1).siblings().css("opacity",0)
+		$(".product ul>li").eq(i).find("h3,.product-show img").addClass("slide-active").end().siblings().find("h3,.product-show img").removeClass("slide-active")
+	})
+	}()
+
